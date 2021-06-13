aab5036b8b98773cd7d37597ac27b7c5297fd826
= <=>
2021-06-13 08:18:24 +0100

    added escape key

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,8 +6,12 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 4f;
-    [SerializeField]
     private Player _player;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    private float _fireRate = 3f;
+    private float _canFire = -1;
+
     private Animator _anim;
     private AudioSource _audioSource;
 
@@ -32,14 +36,29 @@ public class Enemy : MonoBehaviour
 
     void Update()
     {
+        CalcMovement();
+        if(Time.time > _canFire)
+        {
+            _fireRate = Random.Range(3f, 7f);
+            _canFire = Time.time + _fireRate;
+
+            GameObject enemyLaser = Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+            Laser[] lasers = enemyLaser.GetComponentsInChildren<Laser>(); 
+            foreach(Laser l in lasers)
+            {
+                l.AssignEnemyLaser();
+            }
+        }
+    }
+    void CalcMovement()
+    {
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
 
-        if(transform.position.y < -6f)
+        if (transform.position.y < -6f)
         {
             transform.position = new Vector3(transform.position.x, 7f, 0);
         }
     }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
@@ -52,6 +71,7 @@ public class Enemy : MonoBehaviour
             _anim.SetTrigger("OnEnemyDeath");
             _speed = 0f;
             _audioSource.Play();
+            Destroy(GetComponent<Collider2D>());
             Destroy(this.gameObject, 2.5f);
         }
 
@@ -65,6 +85,7 @@ public class Enemy : MonoBehaviour
             _anim.SetTrigger("OnEnemyDeath");
             _speed = 0f;
             _audioSource.Play();
+            Destroy(GetComponent<Collider2D>());
             Destroy(this.gameObject, 2.5f);
         }
     }
--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -6,17 +6,59 @@ public class Laser : MonoBehaviour
 {
     [SerializeField]
     private float _laserSpeed = 8f;
+    private bool _isEnemyLaser = false;
 
     void Update()
+    {
+        if(_isEnemyLaser == true)
+        {
+            EnemyLaser();
+        }
+        else
+        {
+            PlayerLaser();
+        }
+    }
+    void PlayerLaser()
     {
         transform.Translate(Vector3.up * _laserSpeed * Time.deltaTime);
 
         if (transform.position.y > 10f)
         {
-            if(transform.parent != null)
+            if (transform.parent != null)
             {
                 Destroy(transform.parent.gameObject);
             }
+            Destroy(this.gameObject);
+        }
+    }
+
+    void EnemyLaser()
+    {
+        _laserSpeed = 5f;
+        transform.Translate(Vector3.down * _laserSpeed * Time.deltaTime);
+
+        if (transform.position.y < -10f)
+        {
+            if (transform.parent != null)
+            {
+                Destroy(transform.parent.gameObject);
+            }
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void AssignEnemyLaser()
+    {
+        _isEnemyLaser = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player" && _isEnemyLaser == true)
+        {
+            Player player = other.GetComponent<Player>();
+            player.TakeDamage();
             Destroy(this.gameObject);
         }
     }
